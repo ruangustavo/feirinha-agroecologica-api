f326bf4121af88d0a1655afaaac298aa5d22c97c
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2024-02-14 17:46:21 -0300

    feat: add create product route

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,8 +1,9 @@
 import { ProductRepository } from '@/repositories/product.repository'
-import { Product } from '@prisma/client'
+import { Prisma, Product } from '@prisma/client'
 
 export interface IProductService {
   fetchAll(): Promise<Product[]>
+  create(product: Prisma.ProductCreateInput): Promise<Product>
 }
 
 export class ProductService implements IProductService {
@@ -11,5 +12,27 @@ export class ProductService implements IProductService {
   async fetchAll() {
     const products = await this.productRepository.fetchAll()
     return products
+  }
+
+  // NOTE: tem uma razão pra fazer desestruturação do objeto
+  // NOTE: caso um usuário malicioso envie um objeto com propriedades a mais (tipo "id")
+  // NOTE: vai dar um erro de integridade no banco de dados
+  async create({
+    description,
+    image,
+    name,
+    price,
+    stockQuantity,
+    stockUnit,
+  }: Prisma.ProductCreateInput) {
+    const product = await this.productRepository.create({
+      description,
+      image,
+      name,
+      price,
+      stockQuantity,
+      stockUnit,
+    })
+    return product
   }
 }
